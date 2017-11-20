319a9822cd73c4f4321d3bb038832d3122186dab
digitopoi <hubo85@gmail.com>
2017-11-20 10:18:10 -0600

    Finish sql modules

---
+++ 0.10_PURCHASES_INSERT.sql
@@ -0,0 +1,9 @@
+﻿SET IDENTITY_INSERT Purchases On
+INSERT INTO Purchases (PurchaseID, ProductID, CustomerID, Quantity)
+VALUES 
+	(1, 1, 1, 5),
+	(2, 1, 2, 5),	
+	(3, 2, 4, 3);
+GO
+
+SET IDENTITY_INSERT Purchases Off
